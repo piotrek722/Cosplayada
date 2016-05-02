3e4b21ab2c3495f965b2c323db7f7b0d76d82390
piotr <piotrek722@gmail.com>
2016-05-02 13:34:43 +0200

    file init

---
+++ application/src/main/java/pl/edu/agh/tai/CosplayadaApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.tai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosplayadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosplayadaApplication.class, args);
+	}
+}
