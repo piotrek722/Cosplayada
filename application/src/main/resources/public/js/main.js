332ecb9a52b9f313c90b168bcb380c46f80b4827
piotr <piotrek722@gmail.com>
2016-05-06 22:37:45 +0200

    Some html loading in angular

--- application/src/main/resources/public/js/main.js
+++ application/src/main/resources/public/js/main.js
@@ -1,7 +1,23 @@
-
-console.log("Witaj")
-
 $(".nav a").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
+});
+
+var app = angular.module('app',[]);
+
+app.controller('nav', function($scope,$http){
+
+    $scope.site='main.html';
+
+    $scope.changeSite = function(name){
+       $http.get(name).then(function(response){
+           $scope.content = response.data;
+           $scope.site = name;
+       });
+    };
+
+    $scope.getContent = function(){
+        return $scope.site;
+    }
+
 });
