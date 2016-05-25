830eae690bfa42d5db88d2572286098d34901ae0
leeshlay <kijak.shia@gmail.com>
2016-05-25 13:26:12 +0200

    magic window showing up

---
+++ application/src/main/resources/public/js/hello.js
@@ -0,0 +1,6 @@
+angular.module('hello', []).controller('home', function($http) {
+    var self = this;
+    $http.get('resource/').then(function(response) {
+        self.greeting = response.data;
+    })
+});
