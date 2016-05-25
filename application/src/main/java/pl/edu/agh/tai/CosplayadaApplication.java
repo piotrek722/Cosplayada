a6d17ac1ee136d89fb86fa93404750633e9d0f1a
leeshlay <kijak.shia@gmail.com>
2016-05-25 14:02:52 +0200

    logging in as user and showing greeting

--- application/src/main/java/pl/edu/agh/tai/CosplayadaApplication.java
+++ application/src/main/java/pl/edu/agh/tai/CosplayadaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -13,13 +14,18 @@ import java.util.UUID;
 @RestController
 public class CosplayadaApplication {
 
-	@RequestMapping("/resource")
-	public Map<String,Object> home() {
-		Map<String,Object> model = new HashMap<String,Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
-	}
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CosplayadaApplication.class, args);
